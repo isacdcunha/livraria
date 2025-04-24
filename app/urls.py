cc2e02aa58a39f22eb007e68d12154f810c15052
isabelleIFC <isabellecunhaifc@gmail.com>
2025-04-24 11:06:24 -0300

    feat: criação da API para Autor

--- app/urls.py
+++ app/urls.py
@@ -10,8 +10,10 @@ from rest_framework.routers import DefaultRouter
 from core.views import UserViewSet
 from core.views import CategoriaViewSet
 from core.views import EditoraViewSet
+from core.views import AutorViewSet
 
 router = DefaultRouter()
+router.register(r"autor", AutorViewSet)
 router.register(r"categorias", CategoriaViewSet)
 router.register(r"editoras", EditoraViewSet)
 router.register(r'usuarios', UserViewSet, basename='usuarios')
--- core/models/autor.py
+++ core/models/autor.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-class Editora(models.Model):
+class Autor(models.Model):
     nome = models.CharField(max_length=100)
-    site = models.URLField(max_length=200, blank=True, null=True)
+    email = models.EmailField(("Email address"), max_length=100)
 
     def __str__(self):
         return self.nome
