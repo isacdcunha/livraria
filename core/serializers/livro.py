9847fc12ad5bb667f425731250e7fcf7c0b74d41
isacdcunha <isabellecunhaifc@gmail.com>
2025-09-04 11:25:32 -0300

    feat: multiplos serializadores de livro

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -4,12 +4,18 @@ from core.models import Livro
 from uploader.models import Image
 from uploader.serializers import ImageSerializer
 
+
 class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = '__all__'
 
-class LivroListRetrieveSerializer(ModelSerializer):
+class LivroListSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = ('id', 'titulo', 'preco')
+
+class LivroRetrieveSerializer(ModelSerializer):
     class Meta:
         model = Livro
         fields = '__all__'
