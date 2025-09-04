92bd2836b7c78b1197f3aad1b2cdfeb4e1d3fa25
isacdcunha <isabellecunhaifc@gmail.com>
2025-09-04 11:16:54 -0300

    feat: correção da parte 11 (tutorial)

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -1,32 +1,17 @@
-from rest_framework.serializers import ModelSerializer, SlugRelatedField
+from rest_framework.serializers import ModelSerializer
 
 from core.models import Livro
 from uploader.models import Image
 from uploader.serializers import ImageSerializer
 
 class LivroSerializer(ModelSerializer):
-    capa_attachment_key = SlugRelatedField(
-        source='capa',
-        queryset=Image.objects.all(),
-        slug_field='attachment_key',
-        required=False,
-        write_only=True,
-    )
-    capa = ImageSerializer(required=False, read_only=True)
-
     class Meta:
         model = Livro
         fields = '__all__'
 
-class LivroRetrieveSerializer(ModelSerializer):
-    capa = ImageSerializer(required=False)
-
+class LivroListRetrieveSerializer(ModelSerializer):
     class Meta:
         model = Livro
-        fields = "__all__"
+        fields = '__all__'
         depth = 1
 
-class LivroListSerializer(ModelSerializer):
-    class Meta:
-        model = Livro
-        fields = ("id", "titulo", "preco")
--- core/views/livro.py
+++ core/views/livro.py
@@ -1,15 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Livro
-from core.serializers import LivroListSerializer, LivroRetrieveSerializer, LivroSerializer
+from core.serializers import LivroListRetrieveSerializer, LivroSerializer, LivroSerializer
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
 
-    def get_serializer_class(self):  
-        if self.action == "list":
-            return LivroListSerializer
-        elif self.action == "retrieve":
-            return LivroRetrieveSerializer
+    def get_serializer_class(self):
+        if self.action in {'list', 'retrieve'}:
+            return LivroListRetrieveSerializer
         return LivroSerializer
